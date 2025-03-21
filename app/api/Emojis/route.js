06045606c79632073c54087d5062dcb63ee54570
Nitin-Lohumi <lohuminitin@gmail.com>
2025-03-21 19:18:23 +0530

    add

--- app/api/Emojis/route.js
+++ app/api/Emojis/route.js
@@ -8,11 +8,21 @@ export async function POST(req) {
     const { data, language } = await req.json(); // Extract both values properly
 
     if (!data) {
-      return NextResponse.json({ msg: "Please provide emoji input!" }, { status: 400 });
+      return NextResponse.json(
+        { msg: "Please provide emoji input!" },
+        { status: 400 }
+      );
     }
+    if (language == "panjabi") {
+      const prompt = `Translate the given emoji to text in english but it would be panjabi language: ${data}`;
+      const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
+      const result = await model.generateContent(prompt);
 
+      const resp = await result.response.text(); 
+
+      return NextResponse.json({ response: resp, msg: "Success" });
+    }
     const prompt = `Translate the given emoji to text in ${language} language: ${data} in two line `;
-
     const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
     const result = await model.generateContent(prompt);
 
@@ -21,6 +31,9 @@ export async function POST(req) {
     return NextResponse.json({ response: resp, msg: "Success" });
   } catch (error) {
     console.error("API Error:", error);
-    return NextResponse.json({ msg: "Internal Server Error", error: error.message }, { status: 500 });
+    return NextResponse.json(
+      { msg: "Internal Server Error", error: error.message },
+      { status: 500 }
+    );
   }
 }
